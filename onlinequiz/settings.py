292b808de6c4153f7c6d381cee18ca9b9785a82d
Your Name <you@example.com>
2023-06-13 20:10:13 +0700

    done

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 USE_TZ = True 
 ALLOWED_HOSTS = ['0.0.0.0','genidevlms.onrender.com','127.0.0.1','.vercel.app', '.now.sh','www.vutriviet.com']
