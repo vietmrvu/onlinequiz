1e45ae58b68ea3d9599cde25e2d330da2849b95a
Your Name <you@example.com>
2023-06-14 20:48:36 +0700

    done

--- quiz/models.py
+++ quiz/models.py
@@ -80,7 +80,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.body, self.name)
