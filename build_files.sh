f6b0a8496feccc4c48e49ff8027e5d71e8510025
Your Name <you@example.com>
2023-06-15 20:47:30 +0700

    done

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,23 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Create a virtual environment
-# echo "Creating a virtual environment..."
-# python3.9 -m venv venv
-# source venv/bin/activate
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
 
-# # Install the latest version of pip
-# echo "Installing the latest version of pip..."
-# python -m pip install --upgrade pip
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
 
-# # Build the project
-# echo "Building the project..."
-# python -m pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
 
-# # Make migrations
-# echo "Making migrations..."
-# python manage.py makemigrations --noinput
-# python manage.py migrate --noinput
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations
+python manage.py migrate
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic
