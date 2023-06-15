23dd7a668f15b911978dccdf97a4451f6966ecb6
Your Name <you@example.com>
2023-06-15 22:30:06 +0700

    done

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -20,7 +20,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
